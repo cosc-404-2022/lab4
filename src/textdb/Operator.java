ff886622c4bd6bec70cf34562c84f2c12e6175ad
Ramon Lawrence <ramon.lawrence@ubc.ca>
2023-02-02 20:10:05 -0800

    Lab 4

--- src/textdb/Operator.java
+++ src/textdb/Operator.java
@@ -5,26 +5,28 @@ import java.io.*;
 /**
  * A generic class for all operators (scan, projection, join).
  */
-public abstract class Operator
-{
-	protected Operator[] input;			// Input operators
-	protected int numInputs;			// Number of inputs to this operator
-	protected Relation outputRelation;	// Output relation produced by this operator
+public abstract class Operator {
+	protected Operator[] input; 			// Input operators
+	protected int numInputs; 				// Number of inputs to this operator
+	protected Relation outputRelation; 		// Output relation produced by this operator
 
 	// I/O related properties
-	protected int BUFFER_SIZE;			// Number of pages that can be buffered by this operation
-	protected int BLOCKING_FACTOR;		// Number of tuples per page
-	protected int tuplesOutput;			// Total # of tuples output of this operator
-	protected int tuplesRead;			// Total # of tuples read by this operator from its inputs only
-	protected int pagesRead;			// Total # of pages read by this operator from its inputs only
-	protected int internalTupleIOs;		// Total # of tuples read/written by this operator for its own processing (not including read from its sources)
-	protected int internalPageIOs;		// Total # of pages read/written by this operator for its own processing (not including read from its sources)
+	protected int BUFFER_SIZE; 				// Number of pages that can be buffered by this operation
+	protected int BLOCKING_FACTOR; 			// Number of tuples per page
+	protected int tuplesOutput; 			// Total # of tuples output of this operator
+	protected int tuplesRead; 				// Total # of tuples read by this operator from its inputs only
+	protected int pagesRead; 				// Total # of pages read by this operator from its inputs only
+	protected int internalTupleIOs; 		// Total # of tuples read/written by this operator for its own processing (not
+											// including read from its sources)
+	protected int internalPageIOs; 			// Total # of pages read/written by this operator for its own processing (not
+											// including read from its sources)
 
+	Operator() {
+		this(null, 0, 0);
+	}
 
-	Operator()							{	this(null, 0, 0); }
-
-	Operator(Operator []in, int bfr, int bs)
-	{	if (in == null)
+	Operator(Operator[] in, int bfr, int bs) {
+		if (in == null)
 			numInputs = 0;
 		else
 			numInputs = in.length;
@@ -40,40 +42,89 @@ public abstract class Operator
 
 	// Iterator methods
 	abstract public void init() throws IOException;
+
 	abstract public Tuple next() throws IOException;
 
-	public boolean hasNext() throws IOException
-	{	return false; }
+	public boolean hasNext() throws IOException {
+		return false;
+	}
 
-	public void close() throws IOException
-	{	for (int i=0; i < numInputs; i++)
+	public void close() throws IOException {
+		for (int i = 0; i < numInputs; i++)
 			input[i].close();
 	}
 
-	public boolean isBuffered()					// By default, operators are not buffered/threaded
-	{	return false; }
+	public boolean isBuffered() // By default, operators are not buffered/threaded
+	{
+		return false;
+	}
+	
+	public void setOutputRelation(Relation r) {
+		outputRelation = r;
+	}
 
-	//
-	public void setOutputRelation(Relation r)	{ outputRelation = r; }
-	public Relation getOutputRelation()			{ return outputRelation; }
+	public Relation getOutputRelation() {
+		return outputRelation;
+	}
 
 	// Tuple and page counter methods
-	protected void incrementTuplesOutput()		{ tuplesOutput++; }
-	protected void incrementTuplesRead()		{ tuplesRead++; }
-	protected void incrementPagesRead()			{ pagesRead++; }
-	protected void incrementTupleIOs()			{ internalTupleIOs++; /* System.out.println("Tuples output: "+internalTupleIOs);*/}
-	protected void incrementPageIOs()			{ internalPageIOs++; /* System.out.println("Pages output: "+internalPageIOs);*/}
+	protected void incrementTuplesOutput() {
+		tuplesOutput++;
+	}
 
-	protected void incrementTuplesRead(int i)	{ tuplesRead+= i; }
-	protected void incrementPagesRead(int i)	{ pagesRead+= i; }
-	protected void incrementTuplesOutput(int i)	{ tuplesOutput+= i; }
-	protected void incrementTupleIOs(int i)		{ internalTupleIOs+=i; /*System.out.println("Tuples output: "+internalTupleIOs);*/}
-	protected void incrementPageIOs(int i)		{ internalPageIOs+=i; /*System.out.println("Pages output: "+internalPageIOs);*/}
+	protected void incrementTuplesRead() {
+		tuplesRead++;
+	}
 
-	public int getTuplesOutput()				{ return tuplesOutput; }
-	public int getTuplesRead()					{ return tuplesRead; }
-	public int getPagesRead()					{ return pagesRead; }
-	public int getTupleIOs()					{ return internalTupleIOs; }
-	public int getPageIOs()						{ return internalPageIOs; }
+	protected void incrementPagesRead() {
+		pagesRead++;
+	}
+
+	protected void incrementTupleIOs() {
+		internalTupleIOs++;
+		/* System.out.println("Tuples output: "+internalTupleIOs); */}
+
+	protected void incrementPageIOs() {
+		internalPageIOs++;
+		/* System.out.println("Pages output: "+internalPageIOs); */}
+
+	protected void incrementTuplesRead(int i) {
+		tuplesRead += i;
+	}
+
+	protected void incrementPagesRead(int i) {
+		pagesRead += i;
+	}
+
+	protected void incrementTuplesOutput(int i) {
+		tuplesOutput += i;
+	}
+
+	protected void incrementTupleIOs(int i) {
+		internalTupleIOs += i;
+		/* System.out.println("Tuples output: "+internalTupleIOs); */}
+
+	protected void incrementPageIOs(int i) {
+		internalPageIOs += i;
+		/* System.out.println("Pages output: "+internalPageIOs); */}
+
+	public int getTuplesOutput() {
+		return tuplesOutput;
+	}
+
+	public int getTuplesRead() {
+		return tuplesRead;
+	}
+
+	public int getPagesRead() {
+		return pagesRead;
+	}
+
+	public int getTupleIOs() {
+		return internalTupleIOs;
+	}
+
+	public int getPageIOs() {
+		return internalPageIOs;
+	}
 }
-
